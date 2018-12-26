1cf68c072b65f4b05352a644c167337ebea39999
Myron Wu <mwu@abebooks.com>
2018-12-25 21:07:50 -0800

    Getting rid of compiler warnings from day 8

--- day8/src/Main.hs
+++ day8/src/Main.hs
@@ -3,32 +3,36 @@
 module Main where
 
 import           Data.Bifunctor (first)
+import           System.Exit    (die)
 
 
 main :: IO ()
 main = do
   input <- map read . words <$> readFile "input.txt"
-  let (tree, _) = toTree input
+  case toTree input of
+    Nothing        -> die "Unable to parse input into tree."
+    Just (tree, _) -> do
+      putStr "Part 1: "
+      print $ sumMetadata tree
 
-  putStr "Part 1: "
-  print $ sumMetadata tree
+      putStrLn $ "Part 2: " ++ show (value tree)
 
-  putStrLn $ "Part 2: " ++ show (value tree)
+toTree :: [Int] -> Maybe (Tree Int, [Int])
+toTree (numChildren : numMetadata : xs) = do
+  (children, xs') <- toLeaves numChildren xs
+  let (metadata, rest) = splitAt numMetadata xs'
+      value'           = if null children
+                         then sum metadata
+                         else sum $ map (maybe 0 value . elemAt children) metadata
+  Just (TreeNode { value = value', children, metadata }, rest)
 
-toTree :: [Int] -> (Tree Int, [Int])
-toTree (numChildren : numMetadata : xs) = (TreeNode { value = value', children, metadata }, rest)
-  where
-    (children, xs')  = toLeaves numChildren xs
-    (metadata, rest) = splitAt numMetadata xs'
-    value'           = if null children
-                       then sum metadata
-                       else sum $ map (maybe 0 value . elemAt children) metadata
+toTree _ = Nothing
 
-toLeaves :: Int -> [Int] -> ([Tree Int], [Int])
-toLeaves 0 xs = ([], xs)
-toLeaves n xs = first (t :) $ toLeaves (n - 1) rest
-  where
-    (t, rest) = toTree xs
+toLeaves :: Int -> [Int] -> Maybe ([Tree Int], [Int])
+toLeaves 0 xs = Just ([], xs)
+toLeaves n xs = do
+  (t, rest) <- toTree xs
+  first (t :) <$> toLeaves (n - 1) rest
 
 sumMetadata :: Tree Int -> Int
 sumMetadata TreeNode { children, metadata } = sum metadata + sum (map sumMetadata children)
